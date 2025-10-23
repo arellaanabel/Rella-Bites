95fca325c0c29bac7a357897c206a998ff307a35
Arella Elizia Anabel <arellaeliziaanabel@gmail.com>
2025-10-23 12:28:54 +0700

    Add files via upload

---
+++ order.php
@@ -0,0 +1,44 @@
+
+<?php
+$host = "localhost";
+$user = "root";      
+$pass = "";          
+$db   = "RellaBites";
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+$nama    = $_POST['nama'];
+$telepon = $_POST['telepon'];
+$menu    = $_POST['menu'];
+$alamat  = $_POST['alamat'];
+
+$sql = "INSERT INTO orders (nama, telepon, menu, alamat) 
+        VALUES ('$nama', '$telepon', '$menu', '$alamat')";
+if ($conn->query($sql) === TRUE) {
+    $to      = "hello@mompop.cafe";  // ganti dengan email tujuan
+    $subject = "Pesanan Baru dari $nama";
+    $message = "
+        <h2>Pesanan Baru</h2>
+        <p><strong>Nama:</strong> $nama</p>
+        <p><strong>Telepon:</strong> $telepon</p>
+        <p><strong>Menu:</strong> $menu</p>
+        <p><strong>Alamat:</strong> $alamat</p>
+    ";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: Mom & Pop Cafe <no-reply@mompop.cafe>" . "\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<script>alert('Pesanan berhasil dikirim!'); window.location.href='index.html';</script>";
+    } else {
+        echo "<script>alert('Pesanan tersimpan, tapi email gagal terkirim.'); window.location.href='index.html';</script>";
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
